200efc035e91b1bfbc59a058db4a4543352b496a
Rohit <vadlamudi.rohit5@gmail.com>
2023-10-04 19:12:41 +0530

    Strip Frontend and Backend of Flutter

--- backend/models/transcription.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-# Load the English transcription model
-english_model = whisper.load_model("base.en")
-
-# Function to transcribe the audio
-def transcribe(file_path: str) -> str:
-    transcription = english_model.transcribe(file_path, fp16=False)["text"]
-    return transcription
