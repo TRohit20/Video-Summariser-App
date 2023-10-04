f6fd3bf5e8d68275634daabfd8a9515cb9641198
Rohit <vadlamudi.rohit5@gmail.com>
2023-10-04 18:38:11 +0530

    Adding the Function to process Vimeo Videos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+import youtube_dl
 import yt_dlp
 import whisper
 import openai
@@ -34,29 +35,59 @@ def generate_summary(transcript: str) -> str:
     )
     return resp['choices'][0]['message']['content']
  
+
 # Function to download the audio of a YouTube video based on its video ID
-def download(video_id: str) -> str:
+def download_youtube(video_id: str) -> str:
     video_url = f'https://www.youtube.com/watch?v={video_id}'
     output_folder = 'audio'
- 
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
- 
+
     ydl_opts = {
         'format': 'm4a/bestaudio/best',
-        '--ffmpeg-location':'c:\Python311\Lib\site-packages',
-        'outtmpl': os.path.join(output_folder, '%(id)s.%(ext)s'),
+        'outtmpl': os.path.join(output_folder, f'{video_id}.%(ext)s'),
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'm4a',
         }]
     }
- 
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         error_code = ydl.download([video_url])
         if error_code != 0:
             raise Exception('Failed to download video')
- 
+
+    return os.path.join(output_folder, f'{video_id}.m4a')
+
+# Function to download the audio of a Vimeo video based on its video ID
+def download_vimeo(video_id: str) -> str:
+    video_url = f'https://vimeo.com/{video_id}'
+    output_folder = 'audio'
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(output_folder, f'{video_id}.%(ext)s'),
+        'postprocessors': [
+            {
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'm4a',
+            }
+        ],
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': True,
+        'no_warnings': True,
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+        audio_url = info_dict['formats'][0]['url']
+        ydl.download([audio_url])
+
     return os.path.join(output_folder, f'{video_id}.m4a')
  
 st.title('🔗 Summarize Youtube video ')
